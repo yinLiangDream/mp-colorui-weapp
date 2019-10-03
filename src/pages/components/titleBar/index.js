05502407ea5368d44a0a87e9662f10b65e188d00
Yin Liang <s17chucc@163.com>
2019-10-03 23:00:22 +0800

    TitleBar Code

--- src/pages/components/titleBar/index.js
+++ src/pages/components/titleBar/index.js
@@ -1,5 +1,7 @@
 import Taro from '@tarojs/taro'
 import { ClCard, ClLayout, ClTitleBar } from 'mp-colorui'
+import GenerateCode from '../../../components/generateCode'
+import { borderCode, colorCode, iconCode, subTitleCode } from './code'
 
 export default class TitleBar extends Taro.Component {
   config = {
@@ -8,20 +10,28 @@ export default class TitleBar extends Taro.Component {
   render () {
     return (
       <ClLayout>
-        <ClTitleBar title='背景色&文字颜色' textColor='black' type='icon' subTitle='bgColor&textColor'/>
+        <ClTitleBar title='背景色&文字颜色' textColor='black' type='icon' subTitle='bgColor&textColor' />
         <ClCard>
           <ClTitleBar bgColor='green' title='标准背景色' textColor='white' borderColor='white' />
           <ClTitleBar bgColor='light-blue' title='浅色背景色' textColor='black' borderColor='blue' />
           <ClTitleBar bgColor='gradualBlue' title='渐变背景色' textColor='white' borderColor='white' />
         </ClCard>
+        <GenerateCode code={colorCode} />
 
-        <ClTitleBar title='类型' textColor='black' type='icon' subTitle='type'/>
+        <ClTitleBar title='类型' textColor='black' type='icon' subTitle='type' />
         <ClCard>
-          <ClTitleBar title='突出线' type='border-title' />
-          <ClTitleBar title='子标题' subTitle='childTitle' type='icon' />
-          <ClTitleBar type='icon' title='图标' icon='emoji' />
+          <ClTitleBar title='突出线' type='border-title' textColor='blue' borderColor='light-blue' />
         </ClCard>
+        <GenerateCode code={borderCode} />
 
+        <ClCard>
+          <ClTitleBar title='子标题' subTitle='childTitle' type='sub-title' textColor='blue' subTitleColor='red' />
+        </ClCard>
+        <GenerateCode code={subTitleCode} />
+        <ClCard>
+          <ClTitleBar type='icon' title='标题带图标' icon='likefill' iconColor='red' textColor='blue' />
+        </ClCard>
+        <GenerateCode code={iconCode} />
       </ClLayout>
     )
   }
