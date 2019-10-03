36a81a8ce25db56ff46b1979aaf8ba560d245b37
Yin Liang <s17chucc@163.com>
2019-10-03 21:35:23 +0800

    Tag Code Timeline Code

--- src/pages/components/tag/index.js
+++ src/pages/components/tag/index.js
@@ -1,5 +1,7 @@
 import Taro from '@tarojs/taro'
 import { ClCard, ClFlex, ClLayout, ClTag, ClTitleBar } from 'mp-colorui'
+import GenerateCode from '../../../components/generateCode'
+import { clickCode, shapeCode, sizeCode, tagsCode } from './code'
 
 const tags = [
   {
@@ -19,10 +21,11 @@ export default class Tag extends Taro.Component {
   config = {
     navigationBarTitleText: 'Tag 标签'
   }
+
   render () {
     return (
       <ClLayout>
-        <ClTitleBar title='形状' textColor='black' type='icon' subTitle='shape'/>
+        <ClTitleBar title='形状' textColor='black' type='icon' subTitle='shape' />
         <ClCard>
           <ClFlex justify='between'>
             <ClTag tags={tags.slice(0, 1)} shape='radius' />
@@ -30,16 +33,18 @@ export default class Tag extends Taro.Component {
             <ClTag tags={tags.slice(0, 1)} shape='round' />
           </ClFlex>
         </ClCard>
+        <GenerateCode code={shapeCode} />
 
-        <ClTitleBar title='大小' textColor='black' type='icon' subTitle='size'/>
+        <ClTitleBar title='大小' textColor='black' type='icon' subTitle='size' />
         <ClCard>
           <ClFlex justify='around'>
             <ClTag tags={tags.slice(0, 1)} shape='radius' size='small' />
             <ClTag tags={tags.slice(0, 1)} shape='radius' size='normal' />
           </ClFlex>
         </ClCard>
+        <GenerateCode code={sizeCode} />
 
-        <ClTitleBar title='Tag 数组' textColor='black' type='icon' subTitle='array'/>
+        <ClTitleBar title='Tag 数组' textColor='black' type='icon' subTitle='array' />
         <ClCard>
           <ClLayout padding='normal' paddingDirection='bottom'>
             <ClFlex justify='center'>
@@ -55,7 +60,25 @@ export default class Tag extends Taro.Component {
             <ClTag tags={tags} shape='round' size='normal' />
           </ClFlex>
         </ClCard>
+        <GenerateCode code={tagsCode} />
 
+        <ClTitleBar title='点击事件' textColor='black' type='icon' subTitle='array' />
+        <ClCard>
+          <ClFlex justify='center' align='center'>
+            <ClTag
+              tags={tags}
+              shape='round'
+              size='normal'
+              onClick={(index) => {
+                Taro.showToast({
+                  title: tags[index].text,
+                  icon: 'none'
+                })
+              }}
+            />
+          </ClFlex>
+        </ClCard>
+        <GenerateCode code={clickCode} />
       </ClLayout>
     )
   }
--- src/pages/components/timeline/index.js
+++ src/pages/components/timeline/index.js
@@ -1,10 +1,13 @@
 import Taro from '@tarojs/taro'
 import { ClLayout, ClTimeline, ClTitleBar } from 'mp-colorui'
+import GenerateCode from '../../../components/generateCode'
+import { timelineCode } from './code'
 
 export default class Timeline extends Taro.Component {
   config = {
     navigationBarTitleText: 'Timeline 时间轴'
   }
+
   render () {
     return (
       <ClLayout>
@@ -12,24 +15,30 @@ export default class Timeline extends Taro.Component {
         <ClTimeline times={[
           {
             node: '2019'
-          },
-          {
+          }, {
             content: ['我是内容，默认背景']
           }, {
             content: ['我是内容，有背景'],
             bgColor: 'light-blue'
           }, {
             content: ['我是里程碑，我有图标'],
+            title: '2019 年到啦',
+            time: '2019-01-01',
             bgColor: 'light-red',
             icon: 'emoji'
           }, {
-            content: ['我是里程碑，我有图标，我图标有颜色'],
+            node: '2018'
+          }, {
+            content: ['我是里程碑，我有图标，我图标有颜色', '我有多行内容'],
             bgColor: 'red',
             icon: 'emoji',
             iconColor: 'red'
           }
         ]}
         />
+        <ClLayout margin='normal' marginDirection='vertical'>
+          <GenerateCode code={timelineCode} />
+        </ClLayout>
       </ClLayout>
     )
   }
