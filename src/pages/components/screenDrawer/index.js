fcfcb64f072ff8d03676a7cc9b0f6b20ede254ad
Yin Liang <s17chucc@163.com>
2019-10-04 22:54:43 +0800

    screenDrawer code

--- src/pages/components/screenDrawer/index.js
+++ src/pages/components/screenDrawer/index.js
@@ -1,29 +1,44 @@
-import Taro, { useState } from '@tarojs/taro'
-import { ClButton, ClCard, ClLayout, ClScreenDrawer } from 'mp-colorui'
+import Taro, { useState } from "@tarojs/taro";
+import { ClButton, ClCard, ClLayout, ClScreenDrawer } from "mp-colorui";
+import { View } from "@tarojs/components";
+import GenerateCode from "../../../components/generateCode";
+import { demoCode } from "./code";
 
-export default function ScreenDrawer () {
-  const [show, setShow] = useState(false)
-  const mainPage =
-    <ClCard>
-      <ClButton shape='round' long onClick={() => {
-        setShow(true)
-      }}
-      >点我打开全屏抽屉</ClButton>
-    </ClCard>
-  const mainContent =
-    <ClLayout>
+export default function ScreenDrawer() {
+  const [show, setShow] = useState(false);
+  const mainPage = (
+    <View>
       <ClCard>
-        我是内容
+        <ClButton
+          shape="round"
+          long
+          onClick={() => {
+            setShow(true);
+          }}
+        >
+          点我打开全屏抽屉
+        </ClButton>
       </ClCard>
+      <GenerateCode code={demoCode} />
+    </View>
+  );
+  const mainContent = (
+    <ClLayout>
+      <ClCard>我是内容</ClCard>
     </ClLayout>
+  );
   return (
-    <ClScreenDrawer show={show} renderPage={mainPage} renderDrawer={mainContent} onHide={() => {
-      setShow(false)
-    }}
+    <ClScreenDrawer
+      show={show}
+      renderPage={mainPage}
+      renderDrawer={mainContent}
+      onHide={() => {
+        setShow(false);
+      }}
     />
-  )
+  );
 }
 
 ScreenDrawer.config = {
-  navigationBarTitleText: 'ScreenDrawer 全屏抽屉'
-}
+  navigationBarTitleText: "ScreenDrawer 全屏抽屉"
+};
