20165c41c1aaff70bf6c1d12c90cf6fa68086f20
Yin Liang <s17chucc@163.com>
2019-10-04 23:42:22 +0800

    更新记录

--- src/app.js
+++ src/app.js
@@ -1,83 +1,83 @@
-import Taro, { Component } from '@tarojs/taro';
-import '@tarojs/async-await';
+import Taro, { Component } from "@tarojs/taro";
+import "@tarojs/async-await";
 
-import Index from './pages/index';
+import Index from "./pages/index";
 // import router from './router'
 
-import './app.scss';
+import "./app.scss";
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
-if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5') {
-  require('nerv-devtools');
+if (process.env.NODE_ENV !== "production" && process.env.TARO_ENV === "h5") {
+  require("nerv-devtools");
 }
 
 class App extends Component {
   config = {
     pages: [
       // 首页
-      'pages/index/index',
-      'pages/update/index',
+      "pages/index/index",
+      "pages/update/index",
 
       // 组件导航
-      'pages/base/index',
-      'pages/action/index',
-      'pages/form/index',
-      'pages/layout/index',
-      'pages/navigate/index',
-      'pages/view/index',
+      "pages/base/index",
+      "pages/action/index",
+      "pages/form/index",
+      "pages/layout/index",
+      "pages/navigate/index",
+      "pages/view/index",
 
       // 组件
-      'pages/components/icon/index',
-      'pages/components/color/index',
-      'pages/components/text/index',
-      'pages/components/button/index',
-      'pages/components/avatar/index',
-      'pages/components/tag/index',
-      'pages/components/timeline/index',
-      'pages/components/swiper/index',
-      'pages/components/steps/index',
-      'pages/components/searchBar/index',
-      'pages/components/shopBar/index',
-      'pages/components/titleBar/index',
-      'pages/components/modal/index',
-      'pages/components/progress/index',
-      'pages/components/loading/index',
-      'pages/components/input/index',
-      'pages/components/radio/index',
-      'pages/components/checkbox/index',
-      'pages/components/switch/index',
-      'pages/components/select/index',
-      'pages/components/imagePicker/index',
-      'pages/components/layout/index',
-      'pages/components/flex/index',
-      'pages/components/grid/index',
-      'pages/components/menuList/index',
-      'pages/components/card/index',
-      'pages/components/drawer/index',
-      'pages/components/screenDrawer/index',
-      'pages/components/navBar/index',
-      'pages/components/tabBar/index',
-      'pages/components/tabs/index',
-      'pages/components/animation/index',
-      'pages/components/verticalTab/index',
-      'pages/components/accordion/index',
-      'pages/components/floatButton/index',
-      'pages/components/message/index',
-      'pages/components/divider/index',
-      'pages/components/form/index',
-      'pages/components/tip/index',
-      'pages/components/swiperAction/index',
-      'pages/components/actionSheet/index',
-      'pages/components/curtain/index'
+      "pages/components/icon/index",
+      "pages/components/color/index",
+      "pages/components/text/index",
+      "pages/components/button/index",
+      "pages/components/avatar/index",
+      "pages/components/tag/index",
+      "pages/components/timeline/index",
+      "pages/components/swiper/index",
+      "pages/components/steps/index",
+      "pages/components/searchBar/index",
+      "pages/components/shopBar/index",
+      "pages/components/titleBar/index",
+      "pages/components/modal/index",
+      "pages/components/progress/index",
+      "pages/components/loading/index",
+      "pages/components/input/index",
+      "pages/components/radio/index",
+      "pages/components/checkbox/index",
+      "pages/components/switch/index",
+      "pages/components/select/index",
+      "pages/components/imagePicker/index",
+      "pages/components/layout/index",
+      "pages/components/flex/index",
+      "pages/components/grid/index",
+      "pages/components/menuList/index",
+      "pages/components/card/index",
+      "pages/components/drawer/index",
+      "pages/components/screenDrawer/index",
+      "pages/components/navBar/index",
+      "pages/components/tabBar/index",
+      "pages/components/tabs/index",
+      "pages/components/animation/index",
+      "pages/components/verticalTab/index",
+      "pages/components/accordion/index",
+      "pages/components/floatButton/index",
+      "pages/components/message/index",
+      "pages/components/divider/index",
+      "pages/components/form/index",
+      "pages/components/tip/index",
+      "pages/components/swiperAction/index",
+      "pages/components/actionSheet/index",
+      "pages/components/curtain/index"
     ],
     window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
-      navigationBarTextStyle: 'black'
+      backgroundTextStyle: "light",
+      navigationBarBackgroundColor: "#fff",
+      navigationBarTitleText: "WeChat",
+      navigationBarTextStyle: "black"
     },
-    navigateToMiniProgramAppIdList: ['wx8abaf00ee8c3202e']
+    navigateToMiniProgramAppIdList: ["wx8abaf00ee8c3202e"]
   };
 
   componentDidMount() {}
@@ -95,4 +95,4 @@ class App extends Component {
   }
 }
 
-Taro.render(<App />, document.getElementById('app'));
+Taro.render(<App />, document.getElementById("app"));
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,20 +1,21 @@
-import { View } from '@tarojs/components';
-import Taro, { useState, useEffect } from '@tarojs/taro';
+import { View } from "@tarojs/components";
+import Taro, { useState, useEffect } from "@tarojs/taro";
 import {
   ClAnimation,
   ClCard,
   ClFlex,
-  ClFloatButton,
   ClIcon,
   ClLayout,
+  ClModal,
   ClSearchBar,
   ClTabBar,
-  ClText,
-} from 'mp-colorui';
+  ClText
+} from "mp-colorui";
 
-import UserCenter from './components/userCenter'
-import * as menu from '../../constant/menu.js';
-import './index.scss';
+import UserCenter from "./components/userCenter";
+import * as menu from "../../constant/menu.js";
+import "./index.scss";
+import { updateList } from "../../model/index";
 
 const allList = [].concat(
   menu.baseList,
@@ -27,41 +28,43 @@ const allList = [].concat(
 
 export default function Index() {
   // 变量声明区
-  const icons = ['emoji', 'cascades', 'we', 'form', 'apps', 'taxi'];
+  const icons = ["emoji", "cascades", "we", "form", "apps", "taxi"];
   const tabs = [
     {
-      icon: 'home',
-      title: '主页',
+      icon: "home",
+      title: "主页",
       badge: false
     },
     {
-      icon: 'magic',
-      title: '关于',
+      icon: "magic",
+      title: "关于",
       badge: false
     }
   ];
+  const updated = updateList.find(item => item.title);
   const [active, setActive] = useState(0);
-  const [animate, setAnimation] = useState('none');
+  const [animate, setAnimation] = useState("none");
   const [show, setShow] = useState(false);
   const [tempfilter, setTempfilter] = useState([]);
+  const [showUpdate, setShowUpdate] = useState(true);
 
   // 事件声明区
   useEffect(() => {
     let timer = setTimeout(() => {
-      setAnimation('scale-up');
+      setAnimation("scale-up");
       setShow(true);
     }, 800);
     if (
       Taro.getEnv() !== Taro.ENV_TYPE.WEB &&
-      Taro.canIUse('getUpdateManager')
+      Taro.canIUse("getUpdateManager")
     ) {
       let updateManager = Taro.getUpdateManager();
       updateManager.onCheckForUpdate(res => {
         if (res.hasUpdate) {
           updateManager.onUpdateReady(() => {
             Taro.showModal({
-              title: '更新提示',
-              content: '有新版本，是否重启应用？',
+              title: "更新提示",
+              content: "有新版本，是否重启应用？",
               success: ress => {
                 if (ress.confirm) {
                   updateManager.applyUpdate();
@@ -74,8 +77,8 @@ export default function Index() {
           updateManager.onUpdateFailed(() => {
             Taro.hideLoading();
             Taro.showModal({
-              title: '更新失败',
-              content: '新版本更新失败，请检查网络',
+              title: "更新失败",
+              content: "新版本更新失败，请检查网络",
               showCancel: false
             });
           });
@@ -98,24 +101,28 @@ export default function Index() {
       }}
     >
       <ClAnimation type={animate} delay={index / 10}>
-        <ClCard bgColor='white'>
+        <ClCard bgColor="white">
           <View
             style={{
               backgroundImage:
-                'url(https://md-1255362963.cos.ap-chengdu.myqcloud.com/mpcolorui/home.png)',
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
+                "url(https://md-1255362963.cos.ap-chengdu.myqcloud.com/mpcolorui/home.png)",
+              backgroundSize: "cover",
+              backgroundPosition: "center"
             }}
           >
-            <ClFlex justify='between' align='center'>
-              <ClFlex align='center'>
-                <ClIcon iconName={icons[index]} color='cyan' />
-                <ClLayout padding='small' paddingDirection='left'>
-                  <ClText size='xlarge' text={item.name} fontWeight='bold' />
-                  <ClText size='xsmall' textColor='gray' text={item.description} />
+            <ClFlex justify="between" align="center">
+              <ClFlex align="center">
+                <ClIcon iconName={icons[index]} color="cyan" />
+                <ClLayout padding="small" paddingDirection="left">
+                  <ClText size="xlarge" text={item.name} fontWeight="bold" />
+                  <ClText
+                    size="xsmall"
+                    textColor="gray"
+                    text={item.description}
+                  />
                 </ClLayout>
               </ClFlex>
-              <ClIcon iconName='roundrightfill' color='blue' />
+              <ClIcon iconName="roundrightfill" color="blue" />
             </ClFlex>
           </View>
         </ClCard>
@@ -123,15 +130,15 @@ export default function Index() {
     </View>
   ));
   return (
-    <View className='index'>
+    <View className="index">
       {active === 0 ? (
-        <ClLayout padding='xlarge' paddingDirection='vertical'>
+        <ClLayout padding="xlarge" paddingDirection="vertical">
           <ClSearchBar
-            shape='round'
-            bgColor='white'
+            shape="round"
+            bgColor="white"
             fix
-            searchType='none'
-            placeholder='找不到组件在哪？来试试搜索组件吧！'
+            searchType="none"
+            placeholder="找不到组件在哪？来试试搜索组件吧！"
             showResult
             result={tempfilter}
             onTouchResult={index => {
@@ -140,51 +147,74 @@ export default function Index() {
               });
             }}
             onInput={value => {
-              setTempfilter(value !== ''
-              ? allList
-                  .filter(item =>
-                    item.name
-                      .toLowerCase()
-                      .includes(value.toLocaleLowerCase())
-                  )
-                  .map(item => ({
-                    arrow: true,
-                    title: item.name,
-                    key: item.key,
-                    url: `/pages/components/${item.key}/index`
-                  }))
-              : [])
+              setTempfilter(
+                value !== ""
+                  ? allList
+                      .filter(item =>
+                        item.name
+                          .toLowerCase()
+                          .includes(value.toLocaleLowerCase())
+                      )
+                      .map(item => ({
+                        arrow: true,
+                        title: item.name,
+                        key: item.key,
+                        url: `/pages/components/${item.key}/index`
+                      }))
+                  : []
+              );
             }}
           />
         </ClLayout>
       ) : (
-        ''
+        ""
       )}
       {active === 0 ? (
         <ClLayout
-          padding='xlarge'
-          paddingDirection='bottom'
-          margin='xlarge'
-          marginDirection='bottom'
+          padding="xlarge"
+          paddingDirection="bottom"
+          margin="xlarge"
+          marginDirection="bottom"
         >
-          { show ? cardsComponent : ''}
+          {show ? cardsComponent : ""}
         </ClLayout>
       ) : (
-        ''
+        ""
       )}
-      {active === 1 ? <UserCenter /> : ''}
+      {active === 1 ? <UserCenter /> : ""}
       <ClTabBar
         tabs={tabs}
         fix
         active={active}
         onClick={index => {
-          setActive(index)
+          setActive(index);
         }}
       />
+      <ClModal
+        show={showUpdate}
+        close
+        onClose={() => {
+          setShowUpdate(false);
+        }}
+        closeWithShadow
+        onCancel={() => {
+          setShowUpdate(false);
+        }}
+        title={`${updated.title} 版本`}
+      >
+        <ClFlex direction="column">
+          <View style={{ width: "100%" }}>
+            <ClText text={`${updated.time}`} textColor="cyan" align="right" />
+          </View>
+          {updated.content.map(text => (
+            <ClText text={text} size="small" textColor="grey" />
+          ))}
+        </ClFlex>
+      </ClModal>
     </View>
   );
 }
 
 Index.config = {
-  navigationBarTitleText: 'MP-ColorUI'
+  navigationBarTitleText: "MP-ColorUI"
 };
