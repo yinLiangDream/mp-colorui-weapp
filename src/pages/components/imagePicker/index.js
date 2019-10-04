af259be05e8acdee2ae5282fc66101eb20604845
Yin Liang <s17chucc@163.com>
2019-10-04 22:03:58 +0800

    ImagePicker Code

--- src/pages/components/imagePicker/index.js
+++ src/pages/components/imagePicker/index.js
@@ -1,65 +1,97 @@
-import Taro, {useState} from '@tarojs/taro'
-import { ClCard, ClImagePicker, ClLayout, ClTitleBar, ClButton } from 'mp-colorui'
+import Taro, { useState } from "@tarojs/taro";
+import {
+  ClCard,
+  ClImagePicker,
+  ClLayout,
+  ClTitleBar,
+  ClButton
+} from "mp-colorui";
+import GenerateCode from "../../../components/generateCode";
+import { beforeDelCode, maxCode, normalCode } from "./code";
 
-export default function ImagePicker () {
-  const [imgList, setImgList] = useState([])
-  const success = (list) => {
-    setImgList(list)
-  }
+export default function ImagePicker() {
+  const [imgList, setImgList] = useState([]);
+  const success = list => {
+    setImgList(list);
+  };
   return (
     <ClLayout>
-      <ClTitleBar title='默认' textColor='black' type='icon' subTitle='default'/>
+      <ClTitleBar
+        title="默认"
+        textColor="black"
+        type="icon"
+        subTitle="default"
+      />
       <ClCard>
         <ClImagePicker
           chooseImgObj={{
-              success
-            }}
+            success
+          }}
           imgList={imgList}
         />
-        <ClLayout padding='normal' paddingDirection='around'>
+        <ClLayout padding="normal" paddingDirection="around">
           <ClButton
-            shape='round'
+            shape="round"
             long
             onClick={() => {
-              console.log(this.state)
-              setImgList(imgList.map((item) => {
-                item.status = 'loading'
-                return item
-              }))
+              console.log(this.state);
+              setImgList(
+                imgList.map(item => {
+                  item.status = "loading";
+                  return item;
+                })
+              );
               imgList.forEach((item, index) => {
-                item.status = 'loading'
+                item.status = "loading";
                 setTimeout(() => {
-                  item.status = 'success'
-                  if (index === 1) item.status = 'fail'
-                  setImgList(imgList)
-                }, (index + 1) * 1000)
-              })
+                  item.status = "success";
+                  if (index === 1) item.status = "fail";
+                  setImgList(imgList);
+                }, (index + 1) * 1000);
+              });
             }}
-          >开始上传</ClButton>
+          >
+            开始上传
+          </ClButton>
         </ClLayout>
       </ClCard>
+      <GenerateCode code={normalCode} />
 
-      <ClTitleBar title='最多选择 3 张图片' textColor='black' type='icon' subTitle='most'/>
+      <ClTitleBar
+        title="最多选择 3 张图片"
+        textColor="black"
+        type="icon"
+        subTitle="most"
+      />
       <ClCard>
         <ClImagePicker chooseImgObj={{ count: 3 }} />
       </ClCard>
+      <GenerateCode code={maxCode} />
 
-      <ClTitleBar title='删除图片确认操作' textColor='black' type='icon' subTitle='beforeDelete'/>
+      <ClTitleBar
+        title="删除图片确认操作"
+        textColor="black"
+        type="icon"
+        subTitle="beforeDelete"
+      />
       <ClCard>
-        <ClImagePicker chooseImgObj={{ count: 3 }} beforeDel={async () => {
-          const res = await Taro.showModal({
-            title: '您确认要删除吗'
-          })
-          console.log(res)
-          if (res.confirm) return true
-          if (res.cancel) return false
-        }}
+        <ClImagePicker
+          chooseImgObj={{ count: 3 }}
+          beforeDel={async () => {
+            const res = await Taro.showModal({
+              title: "您确认要删除吗"
+            });
+            console.log(res);
+            if (res.confirm) return true;
+            if (res.cancel) return false;
+          }}
         />
       </ClCard>
+      <GenerateCode code={beforeDelCode} />
     </ClLayout>
-  )
+  );
 }
 
 ImagePicker.config = {
-  navigationBarTitleText: 'ImagePicker 图片选择器'
-}
+  navigationBarTitleText: "ImagePicker 图片选择器"
+};
