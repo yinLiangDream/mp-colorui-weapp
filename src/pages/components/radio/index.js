c002b59098ffe13e7c77ba553950e9d96e6b57c9
Yin Liang <s17chucc@163.com>
2019-10-04 21:29:56 +0800

    Radio Code

--- src/pages/components/radio/index.js
+++ src/pages/components/radio/index.js
@@ -1,66 +1,127 @@
-import Taro, { useState } from '@tarojs/taro'
-import { ClCard, ClLayout, ClRadio, ClTitleBar } from 'mp-colorui'
+import Taro, { useState } from "@tarojs/taro";
+import { ClCard, ClLayout, ClRadio, ClTitleBar } from "mp-colorui";
+import GenerateCode from "../../../components/generateCode";
+import { colorCode, positionCode, shapeCode, typeCode } from "./code";
 
-const radioGroup = [{
-  key: '男生',
-  value: 'male'
-}, {
-  key: '女生',
-  value: 'female'
-}]
+const radioGroup = [
+  {
+    key: "男生",
+    value: "male"
+  },
+  {
+    key: "女生",
+    value: "female"
+  }
+];
 
-export default function Radio () {
-  const [checkedValue, setCheckedValue] = useState('male')
+export default function Radio() {
+  const [checkedValue, setCheckedValue] = useState("male");
   return (
     <ClLayout>
-      <ClTitleBar title='类型' textColor='black' type='icon' subTitle='type'/>
+      <ClTitleBar title="类型" textColor="black" type="icon" subTitle="type" />
       <ClCard>
-        <ClRadio type='normal' title='normal' radioGroup={radioGroup} checkedValue={checkedValue} />
+        <ClRadio
+          type="normal"
+          title="normal"
+          radioGroup={radioGroup}
+          checkedValue={checkedValue}
+        />
       </ClCard>
       <ClCard>
-        <ClRadio type='form' title='性别' radioGroup={radioGroup} checkedValue={checkedValue} />
+        <ClRadio
+          type="form"
+          title="性别"
+          radioGroup={radioGroup}
+          checkedValue={checkedValue}
+        />
       </ClCard>
       <ClCard>
-        <ClRadio type='list' title='性别' radioGroup={radioGroup} checkedValue={checkedValue} />
+        <ClRadio
+          type="list"
+          title="性别"
+          radioGroup={radioGroup}
+          checkedValue={checkedValue}
+        />
       </ClCard>
+      <GenerateCode code={typeCode} />
 
-      <ClTitleBar title='方向' textColor='black' type='icon' subTitle='direction'/>
+      <ClTitleBar
+        title="方向"
+        textColor="black"
+        type="icon"
+        subTitle="direction"
+      />
       <ClCard>
-        <ClRadio type='normal' title='normal' radioGroup={radioGroup} checkedValue={checkedValue}
-          direction='horizontal'
+        <ClRadio
+          type="form"
+          title="性别"
+          radioGroup={radioGroup}
+          checkedValue={checkedValue}
+          direction="horizontal"
         />
       </ClCard>
       <ClCard>
-        <ClRadio type='form' title='性别' radioGroup={radioGroup} checkedValue={checkedValue} direction='horizontal' />
+        <ClRadio
+          type="form"
+          title="性别"
+          radioGroup={radioGroup}
+          checkedValue={checkedValue}
+          direction="vertical"
+        />
       </ClCard>
+      <GenerateCode code={positionCode} />
 
-      <ClTitleBar title='颜色' textColor='black' type='icon' subTitle='color'/>
+      <ClTitleBar title="颜色" textColor="black" type="icon" subTitle="color" />
       <ClCard>
-        <ClRadio type='normal' title='normal' radioGroup={radioGroup} checkedValue={checkedValue} direction='horizontal'
-          color='blue'
+        <ClRadio
+          type="normal"
+          title="normal"
+          radioGroup={radioGroup}
+          checkedValue={checkedValue}
+          direction="horizontal"
+          color="blue"
         />
       </ClCard>
       <ClCard>
-        <ClRadio type='form' title='性别' radioGroup={radioGroup} checkedValue={checkedValue} direction='horizontal'
-          color='red'
+        <ClRadio
+          type="form"
+          title="性别"
+          radioGroup={radioGroup}
+          checkedValue={checkedValue}
+          direction="horizontal"
+          color="red"
         />
       </ClCard>
+      <GenerateCode code={colorCode} />
 
-      <ClTitleBar title='形状' textColor='black' type='icon' subTitle='shape'/>
+      <ClTitleBar title="形状" textColor="black" type="icon" subTitle="shape" />
       <ClCard>
-        <ClRadio type='normal' shape='normal' title='normal' radioGroup={radioGroup} checkedValue={checkedValue}
-          direction='horizontal' color='blue'
+        <ClRadio
+          type="normal"
+          shape="normal"
+          title="normal"
+          radioGroup={radioGroup}
+          checkedValue={checkedValue}
+          direction="horizontal"
+          color="blue"
         />
       </ClCard>
       <ClCard>
-        <ClRadio type='form' shape='radio' title='性别' radioGroup={radioGroup} checkedValue={checkedValue}
-          direction='horizontal' color='red'
+        <ClRadio
+          type="form"
+          shape="radio"
+          title="性别"
+          radioGroup={radioGroup}
+          checkedValue={checkedValue}
+          direction="horizontal"
+          color="red"
         />
       </ClCard>
+      <GenerateCode code={shapeCode} />
     </ClLayout>
-  )
+  );
 }
 
 Radio.config = {
-  navigationBarTitleText: 'Radio 单选按钮'
-}
+  navigationBarTitleText: "Radio 单选按钮"
+};
