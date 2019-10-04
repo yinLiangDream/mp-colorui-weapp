040cafc7907219bfc0c2478ee81f863ed71f3012
Yin Liang <s17chucc@163.com>
2019-10-04 21:38:39 +0800

    switch Code

--- src/pages/components/switch/index.js
+++ src/pages/components/switch/index.js
@@ -1,36 +1,51 @@
-import Taro from '@tarojs/taro'
-import { ClCard, ClLayout, ClSwitch, ClTitleBar } from 'mp-colorui'
+import Taro from "@tarojs/taro";
+import { ClCard, ClLayout, ClSwitch, ClTitleBar } from "mp-colorui";
+import GenerateCode from "../../../components/generateCode";
+import { colorCode, shapeCode, typeCode } from "./code";
 
 export default function Switch() {
   return (
     <ClLayout>
-      <ClTitleBar title='类型' textColor='black' type='icon' subTitle='type'/>
+      <ClTitleBar title="类型" textColor="black" type="icon" subTitle="type" />
       <ClCard>
-        <ClSwitch type='normal' />
+        <ClSwitch type="normal" />
       </ClCard>
       <ClCard>
-        <ClSwitch type='form' title={'~\(≧▽≦)/~啦啦啦'} />
+        <ClSwitch type="form" title="~(≧▽≦)/~啦啦啦" />
       </ClCard>
+      <GenerateCode code={typeCode} />
 
-      <ClTitleBar title='形状' textColor='black' type='icon' subTitle='shape'/>
+      <ClTitleBar title="形状" textColor="black" type="icon" subTitle="shape" />
       <ClCard>
-        <ClSwitch type='form' title={'~\(≧▽≦)/~啦啦啦'} shape='normal' />
+        <ClSwitch type="form" title="~(≧▽≦)/~啦啦啦" shape="normal" />
       </ClCard>
       <ClCard>
-        <ClSwitch type='form' title={'~\(≧▽≦)/~啦啦啦'} shape='radius' />
+        <ClSwitch type="form" title="~(≧▽≦)/~啦啦啦" shape="radius" />
       </ClCard>
+      <GenerateCode code={shapeCode} />
 
-      <ClTitleBar title='颜色' textColor='black' type='icon' subTitle='color'/>
+      <ClTitleBar title="颜色" textColor="black" type="icon" subTitle="color" />
       <ClCard>
-        <ClSwitch type='form' title={'~\(≧▽≦)/~啦啦啦'} shape='normal' color='cyan' />
+        <ClSwitch
+          type="form"
+          title="~(≧▽≦)/~啦啦啦"
+          shape="normal"
+          color="cyan"
+        />
       </ClCard>
       <ClCard>
-        <ClSwitch type='form' title={'~\(≧▽≦)/~啦啦啦'} shape='radius' color='red' />
+        <ClSwitch
+          type="form"
+          title="~(≧▽≦)/~啦啦啦"
+          shape="radius"
+          color="red"
+        />
       </ClCard>
+      <GenerateCode code={colorCode} />
     </ClLayout>
-  )
+  );
 }
 
 Switch.config = {
-  navigationBarTitleText: 'Switch 开关'
-}
+  navigationBarTitleText: "Switch 开关"
+};
