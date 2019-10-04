ab100c3adb64d6d3ffd2a33bbf0915ea9ee67fc6
Yin Liang <s17chucc@163.com>
2019-10-04 20:27:43 +0800

    form Code

--- src/pages/components/form/index.js
+++ src/pages/components/form/index.js
@@ -1,4 +1,4 @@
-import Taro from '@tarojs/taro';
+import Taro from "@tarojs/taro";
 import {
   ClLayout,
   ClForm,
@@ -9,11 +9,13 @@ import {
   ClCard,
   ClMessage,
   ClCheckbox
-} from 'mp-colorui'
+} from "mp-colorui";
+import GenerateCode from "../../../components/generateCode";
+import { fullCode } from "./code";
 
 export default class Cl_Form extends Taro.Component {
   static config = {
-    navigationBarTitleText: 'Form 表单'
+    navigationBarTitleText: "Form 表单"
   };
   ref = null;
   refFunc = node => {
@@ -22,73 +24,78 @@ export default class Cl_Form extends Taro.Component {
   handleSubmit() {
     this.ref.validate(success => {
       this.setState({
-        message: `数据校验${success ? '通过' : '未通过' }`,
-        messageType: success ? 'success' : 'error',
+        message: `数据校验${success ? "通过" : "未通过"}`,
+        messageType: success ? "success" : "error",
         showMessage: true
-      })
+      });
     });
   }
   state = {
     model: {
-      name: '我是名字',
-      phone: '188',
+      name: "我是名字",
+      phone: "188",
       hobby: []
     },
-    message: '',
+    message: "",
     showMessage: false,
-    messageType: 'info'
+    messageType: "info"
   };
   render() {
     const rules = {
       name(rule, value, callback) {
         if (!rule.required(value)) {
-          callback('姓名不能为空');
+          callback("姓名不能为空");
           return false;
         }
         if (value.length > 5) {
-          callback('姓名长度不能超过 5');
+          callback("姓名长度不能超过 5");
           return false;
         }
         return true;
       },
       phone(rule, value, callback) {
         if (!rule.required(value)) {
-          callback('手机号码不能为空');
+          callback("手机号码不能为空");
           return false;
         }
         if (!rule.phone(value)) {
-          callback('手机号码不正确');
+          callback("手机号码不正确");
           return false;
         }
         return true;
       },
       hobby(rule, value, callback) {
         if (value.length < 2) {
-          callback('至少选择 2 项')
-          return false
+          callback("至少选择 2 项");
+          return false;
         }
-        return true
+        return true;
       }
     };
     const { model, showMessage, message, messageType } = this.state;
     return (
       <ClLayout>
-        <ClMessage message={message} show={showMessage} type={messageType} onClose={() => {
-          this.setState({
-            showMessage: false
-          })
-        }}
+        <ClMessage
+          message={message}
+          show={showMessage}
+          type={messageType}
+          onClose={() => {
+            this.setState({
+              showMessage: false
+            });
+          }}
         />
-        <ClTitleBar title='实时校验&&失焦校验' textColor='black' type='icon' />
+        <ClTitleBar title="实时校验&&失焦校验" textColor="black" type="icon" />
         <ClCard>
-          由于小程序的 view 没有失焦事件，目前组件库只能做到 model 里面的数据更新就会进行校验，如果想做到失焦校验请手动在 Input 组件下绑定 onBlur 事件，在此事件里面进行数据更新。
-          具体用法请看文档。
+          由于小程序的 view 没有失焦事件，目前组件库只能做到 model
+          里面的数据更新就会进行校验，如果想做到失焦校验请手动在 Input
+          组件下绑定 onBlur 事件，在此事件里面进行数据更新。 具体用法请看文档。
         </ClCard>
         <ClCard>
           <ClForm ref={this.refFunc} model={model} rules={rules}>
-            <ClFormItem prop='name' required>
+            <ClFormItem prop="name" required>
               <ClInput
-                title='姓名（失焦）'
+                title="姓名（失焦）"
                 value={model.name}
                 onBlur={value => {
                   this.setState({
@@ -100,9 +107,9 @@ export default class Cl_Form extends Taro.Component {
                 }}
               />
             </ClFormItem>
-            <ClFormItem prop='phone' required>
+            <ClFormItem prop="phone" required>
               <ClInput
-                title='手机号码（实时）'
+                title="手机号码（实时）"
                 value={model.phone}
                 onChange={value => {
                   this.setState({
@@ -114,34 +121,46 @@ export default class Cl_Form extends Taro.Component {
                 }}
               />
             </ClFormItem>
-            <ClFormItem prop='hobby'>
-              <ClCheckbox type='form' shape='round' title='选择爱好' checkboxGroup={[
-                {
-                  key: '下棋',
-                  value: 'xiaqi'
-                },
-                {
-                  key: '画画',
-                  value: 'huahua'
-                },
-                {
-                  key: '唱歌',
-                  value: 'singsong'
-                }
-              ]}
+            <ClFormItem prop="hobby">
+              <ClCheckbox
+                type="form"
+                shape="round"
+                title="选择爱好"
+                checkboxGroup={[
+                  {
+                    key: "下棋",
+                    value: "xiaqi"
+                  },
+                  {
+                    key: "画画",
+                    value: "huahua"
+                  },
+                  {
+                    key: "唱歌",
+                    value: "singsong"
+                  }
+                ]}
                 onChange={value => {
-                            this.setState({
-                              model: {
-                                ...model,
-                                hobby: value
-                              }
-                            })
-                          }}
+                  this.setState({
+                    model: {
+                      ...model,
+                      hobby: value
+                    }
+                  });
+                }}
               />
             </ClFormItem>
           </ClForm>
         </ClCard>
-        <ClButton text='校验' shape='round' long onClick={this.handleSubmit.bind(this)} />
+        <ClLayout padding="normal" paddingDirection="around">
+          <ClButton
+            text="校验"
+            shape="round"
+            long
+            onClick={this.handleSubmit.bind(this)}
+          />
+        </ClLayout>
+        <GenerateCode code={fullCode} />
       </ClLayout>
     );
   }
